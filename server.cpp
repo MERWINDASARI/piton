cd44d5492161e872b30c3b1a1726e6867484242f
MERWINDASARI <dasarimerwinmighty@gmail.com>
2024-08-25 19:06:31 +0530

    Now a single client can send multiple messages in single connection

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <winsock2.h>
+#include <cassert>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,22 +19,79 @@ static void die(const char *msg) {
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
 }
-
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = recv(connfd, rbuf, sizeof(rbuf) - 1,0);
-    if (n < 0) {
-        msg("read() error");
-        return;
+const size_t k_max_msg = 4096;
+static int32_t read_full(int fd, char *buf, size_t n){
+    while(n>0){
+         ssize_t rv = recv(fd, buf, n,0);
+         if(rv<=0){
+            //error or unexpected EOF
+            return -1;
+         }
+         assert((size_t)rv <= n);
+         n-=(size_t)rv;
+         buf+=rv;
     }
-    printf("client says: %s\n", rbuf);
+    return 0;
+}
 
-    char wbuf[] = "read your msg";
-    send(connfd, wbuf, strlen(wbuf),0);
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = send(fd, buf, n,0);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd) {
+    //4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+
+    int32_t err = read_full(connfd, rbuf, 4);
+
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "read your msg ";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main(){
 
+    //windows related intialization
     WSADATA wsaData;
     int iResult;
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
@@ -55,14 +113,11 @@ int main(){
     addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
 
+
+    //this is more like exception 
     if (rv == SOCKET_ERROR) {
         printf("bind failed with error: %d\n", WSAGetLastError());
         WSACleanup();
-        return 1;
-    }
-
-    //this is more like exception 
-    if (rv) {
         die("bind()");
     }
 
@@ -82,9 +137,17 @@ int main(){
             continue;   // error
         }
 
-        do_something(connfd);
+        while(true){
+            //server serves one client at ones
+            int32_t err = one_request(connfd);
+            if(err){
+                break;
+            }
+        }
+
         closesocket(connfd);
     }
     WSACleanup();
+    return 0;
 
 }
